91fcbf7ac721779f954321facf615588d5e979a2
Claire Poulsen <claire.e.poulsen@gmail.com>
2023-12-07 14:34:35 -0700

    Added additional functionality for OutOfRange and clarifying comments

--- main.cpp
+++ main.cpp
@@ -2,32 +2,43 @@
 #include "Exceptions.h"
 using namespace std;
 
+// This function is declared to throw an exception, basically making it one big 'try' block
 double safeDivide(int top, int bottom) throw (DivideByZero);
 
 int main() {
 
 	int i = 0;
 	double result = 0.0;
+	char testAgain = 'y';
 
-	try {
-		cout << "Enter an integer: ";
-		cin >> i;
-		if (cin.fail()) throw NotInt("Not an integer.");
-		cout << "You entered: " << i << endl;
-		result = safeDivide(17, i);
-		cout << "17 / " << i << " = " << result << endl;
-	}
-	catch (NotInt e) {
-		cout << e.whatHappened() << endl;
-		cin.clear();
-		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	}
-	catch (OutOfRange e) {
-		if (e.isSpecified()) cout << e.getValue() << " is ";
-		cout << "not permitted." << endl;
-	}
-	catch (DivideByZero e) {
-		cout << "Divide by zero error." << endl;
+	while (testAgain == 'y') {
+		try {
+			cout << "Enter an integer between -100 and 100: ";
+			cin >> i;
+			if (cin.fail()) throw NotInt("Not an integer."); // if the user enters a value that is not an int
+
+			if (i > 100) throw OutOfRange(i); // if the user enters a value above the desired range, pass the value
+
+			if (i < -100) throw OutOfRange(); // if the user enters a value below the desired range, throw a general out of range error
+			cout << "You entered: " << i << endl;
+			result = safeDivide(17, i);
+			cout << "17 / " << i << " = " << result << endl;
+		}
+		catch (NotInt e) { // whatHappened() returns the string that is put in the throw
+			cout << e.whatHappened() << endl;
+			cin.clear();
+			cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+		catch (OutOfRange e) { // OutOfRange returns the value it was passed, saying not permitted
+			if (e.isSpecified()) cout << e.getValue() << " is not permitted." << endl;
+
+			if (!e.isSpecified()) cout << "Please only enter values between -100 and 100." << endl;
+		}
+		catch (DivideByZero e) {
+			cout << "Divide by zero error." << endl;
+		}
+		cout << "Would you like to test again?(y/n): ";
+		cin >> testAgain;
 	}
 
 	return 0;
@@ -36,6 +47,6 @@ int main() {
 double safeDivide(int top, int bottom) throw(DivideByZero) {
 
 	if (bottom == 0)
-		throw DivideByZero();
-	return top/static_cast<double>(bottom);
+		throw DivideByZero(); // throws the error to be caught higher up in main()
+	return top/static_cast<double>(bottom); // or returns the value (static cast the int to a double to divide properly)
 }
